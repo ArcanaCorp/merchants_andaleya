17f42fd62d176bfa9aea9a7c9a5658c562124424
Jhans-15 <72289225+JFrancodev12@users.noreply.github.com>
2025-10-14 14:40:58 -0500

    upgrade initial project

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import { createRoot } from "react-dom/client";
 import Providers from "./app/providers";
+import { RouterProvider } from "react-router-dom";
+import router from "./app/routers";
 
 createRoot(document.getElementById('root')).render(
     <>
         <Providers>
-            <
+            <RouterProvider router={router} />
         </Providers>
     </>
 )
